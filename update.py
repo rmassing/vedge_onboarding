61f722e2ea2a63cf6580350f1d1eeb89a1f3e276
rmassing <rmassing@cisco.com>
2020-07-24 09:55:55 -0400

    Update update.py

--- update.py
+++ update.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------
+# adding new line
 
 from eman import Eman
 from secrets import USERNAME, PASSWORD
